f7448544124101a2d511f54513aaadd33afe1bfc
Renat Husnetdinov <renatilshatovich@gmail.com>
2023-08-30 15:08:04 +0300

    Add Tower Builder

---
+++ Assets/Script/TowerBuilder.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class TowerBuilder : MonoBehaviour
+{
+    [SerializeField] private int _levelCount;
+    [SerializeField] private GameObject _beam;
+
+    private void Start()
+    {
+       Build(); 
+    }
+
+    private void Build()
+    {
+        GameObject beam = Instantiate(_beam, transform);
+        beam.transform.localScale = new Vector3(1, _levelCount / 2f, 1);
+    }
+}
